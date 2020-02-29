78d9880e9ea9fcd1f3f880341c237adf96c8fcd1
pickk <hi@pickk.co>
2020-02-29 16:02:54 -0600

    Added s3 upload

--- details-csv-generator.py
+++ details-csv-generator.py
@@ -1,13 +1,16 @@
-# import pandas as pd
-import modin.pandas as pd
+import pandas as pd
 import glob, os
+import boto3
 
-import_dir = 'imports/Detail/year=2020/month=1'
+s3 = boto3.resource('s3')
 
-for subdir, dirs, files in os.walk('./' + import_dir):
+import_dir = 'imports/Detail/'
+date_folder = 'year=2020/month=2/'
+
+for subdir, dirs, files in os.walk('./' + import_dir + date_folder):
   for filename in files:
     # Get voter details and clean
-    details = pd.read_csv( import_dir + filename, sep="\t", header=None, low_memory=False)
+    details = pd.read_csv( import_dir + date_folder + filename, sep="\t", header=None, low_memory=False)
     details.columns = ["County Code", "Voter ID", "Name Last", "Name Suffix", "Name First", "Name Middle", "Requested public records", "Residence Address Line 1", "Residence Address Line 2", "Residence City (USPS)", "Residence State", "Residence Zipcode", "Mailing Address Line 1", "Mailing Address Line 2", "Mailing Address Line", "Mailing City", "Mailing State", "Mailing Zipcode", "Mailing Country", "Gender", "Race", "Birth Date", "Registration Date", "Party Affiliation", "Precinct", "Precinct Group", "Precinct Split", "Precinct Suffix", "Voter Status", "Congressional District", "House District", "Senate District", "County Commission District", "School Board District", "Daytime Area Code ", "Daytime Phone Number", "Daytime Phone Extension", "Email address"]
 
     # mailer = details.copy()
@@ -18,8 +21,12 @@ for subdir, dirs, files in os.walk('./' + import_dir):
     county = filename[0:3]
     print(county)
 
-    directory = "exports/Detail/county=" + county + "/year=2020/month=1/"
+    # Save locally
+    directory = "exports/Detail/" + date_folder
     if not os.path.exists(directory):
       os.makedirs(directory)
 
     details.to_csv(directory + county + '.csv', sep='\t', index=False)
+
+    #Upload to S3
+    s3.meta.client.upload_file(directory + county + '.csv', 'fldemdatalake', 'CSV/Detail/' + date_folder + county + '.csv')
--- history-csv-generator.py
+++ history-csv-generator.py
@@ -1,11 +1,25 @@
 import pandas as pd
 import glob, os
+import boto3
 
-for subdir, dirs, files in os.walk('./imports/History'):
+s3 = boto3.resource('s3')
+
+import_dir = 'imports/History/'
+date_folder = 'year=2020/month=2/'
+
+for subdir, dirs, files in os.walk('./' + import_dir + date_folder):
   for filename in files:
     # Get voter details and clean
-    details = pd.read_csv('imports/History/' + filename, sep="\t", header=None, low_memory=False)
+    details = pd.read_csv( import_dir + date_folder + filename, sep="\t", header=None, low_memory=False)
     details.columns = ["County Code", "Voter ID", "Election Date", "Election Type", "History Code"]    
     county = filename[0:3]
     print(county)
-    details.to_csv('exports/History/' + county + '20190312.csv', index=False)
+
+    directory = "exports/History/" + date_folder
+    if not os.path.exists(directory):
+      os.makedirs(directory)
+
+    details.to_csv(directory + county + '.csv', sep='\t', index=False)
+    
+    #Upload to S3
+    s3.meta.client.upload_file(directory + county + '.csv', 'fldemdatalake', 'CSV/History/' + date_folder + county + '.csv')
